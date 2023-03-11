04b6c060166aa72533ecd30ca22b398fb35f4ef3
officials3vo <shaqbar1498@outlook.com>
2023-03-11 03:25:54 -0500

    update the server url fix

--- client/script.js
+++ client/script.js
@@ -148,10 +148,10 @@ loader(messageDiv);
 // the header is going to be an object containing Content-type
 //finally, we have to pass our valubale body, and we pass in an object where we say 'data.get('prompt')'
 //which is our data or msg coming from our text area element on the screen
-const response = await fetch('https://talkbot-4wlm.onrender.com/', {
+const response = await fetch('https://talkbot-4wlm.onrender.com', {
   method: 'POST',
   headers: {
-    'Content-type': 'application/json',
+    'Content-Type': 'application/json',
   },
   body: JSON.stringify({
     prompt: data.get('prompt')
